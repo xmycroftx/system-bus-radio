3b662e133e81f4bcfc9836c6ae5cdbdd0ddeeeac
Eugene Bujak <hmage@hmage.net>
2016-03-01 22:27:35 +0300

    Compiles on both linux and mach

--- main.c
+++ main.c
@@ -4,9 +4,43 @@
 
 #include <stdio.h>
 #include <emmintrin.h>
+#include <inttypes.h>
+#include <time.h>
+#ifdef __MACH__
 #include <mach/mach_traps.h>
 #include <mach/mach_time.h>
+#endif
 #include <math.h>
+
+#ifndef NSEC_PER_SEC
+#define NSEC_PER_SEC 1000000000ull
+#endif
+
+#ifndef __MACH__
+typedef struct timespec mach_timespec_t;
+typedef unsigned int mach_port_t;
+
+static inline uint64_t mach_absolute_time(void) {
+    mach_timespec_t tp;
+    int res = clock_gettime(CLOCK_MONOTONIC_RAW, &tp);
+    if (res < 0) {
+        perror("clock_gettime");
+        exit(1);
+    }
+    uint64_t result = tp.tv_sec * NSEC_PER_SEC;
+    result += tp.tv_nsec;
+    return result;
+}
+
+static inline void nanosleep_for(time_t sec, long nsec, mach_timespec_t *remain) {
+    mach_timespec_t req = { sec, nsec };
+    int res = nanosleep(&req, remain);
+    if (res < 0) {
+        perror("nanosleep");
+        exit(1);
+    }
+}
+#endif // __MACH__
 
 __m128i reg;
 __m128i reg_zero;
@@ -28,17 +62,23 @@ static inline void square_am_signal(float time, float frequency) {
             _mm_stream_si128(&reg, reg_one);
             _mm_stream_si128(&reg, reg_zero);
         }
+#ifdef __MACH__
         clock_sleep_trap(clock_port, TIME_ABSOLUTE, reset / NSEC_PER_SEC, reset % NSEC_PER_SEC, &remain);
+#else
+        nanosleep_for((period / 2) / NSEC_PER_SEC, (period / 2) % NSEC_PER_SEC, &remain);
+#endif
         start = reset;
     }
 }
 
 int main()
 {
+#ifdef __MACH__
     mach_timebase_info_data_t theTimeBaseInfo;
     mach_timebase_info(&theTimeBaseInfo);
     puts("TESTING TIME BASE: the following should be 1 / 1");
     printf("  Mach base: %u / %u nanoseconds\n\n", theTimeBaseInfo.numer, theTimeBaseInfo.denom);
+#endif
 
     uint64_t start = mach_absolute_time();
     uint64_t end = mach_absolute_time();
