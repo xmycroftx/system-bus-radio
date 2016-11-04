cb8231cd915f7e1db259bc9f1bef7f3cf03f7600
Elliot Gerchak <e@dementedlab.com>
2016-11-04 04:39:00 +0000

    Added a delay function and fixed time being *1000.
    There was a bug before, that caused the window to freeze up (seemingly forever). This was due to the fact that the times in milliseconds were being treated as time in seconds. This has been fixed with a *.0001 function.
    
    Also, a block of code for the "delay" function has been added. Delays between same notes in the included demo song have also been added.

--- In Javascript/airgap.js
+++ In Javascript/airgap.js
@@ -33,13 +33,19 @@ function start() {
 	var line, time, freq;
 	for (var i = 0; i < length; i++) {
 		line = song[i].split(" ");
-		if (line[1] == "0") {
-			// delay
+		if (+line[1] == 0) {
+			pause(line[0]);
 		}
 		else {
 			freq = +line[1];
-			time = +line[0];
+			time = (+line[0])*.001;
 			square_am_signal(time, freq);
 		}
 	}
+}
+
+function pause(time) {
+	window.logs.value += "\nPaused / " + time*.001 + " seconds";
+	var dt = new Date();
+	while ((new Date()) - dt <= time) { /* Do nothing */ }
 }
