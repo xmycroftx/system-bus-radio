b47a9eccd379b36ce96da3fe7ee91a74852acde1
Elliot Gerchak <emgerchak@gmail.com>
2016-11-23 23:12:26 -0500

    Improvements to html, css, and js (#30)
    * Improved formatting
    
    * removed unused and unrelated  CSS classes
    
    * removed more unused CSS
    
    * removed red outline around text box
    
    * improved link styling
    
    * Added a delay function and fixed time being *1000.
    
    There was a bug before, that caused the window to freeze up (seemingly forever). This was due to the fact that the times in milliseconds were being treated as time in seconds. This has been fixed with a *.0001 function.
    
    Also, a block of code for the "delay" function has been added. Delays between same notes in the included demo song have also been added.
    
    * fixed description meta
    
    * created var for window log

--- In Javascript/airgap.js
+++ In Javascript/airgap.js
@@ -8,9 +8,10 @@ function now() {
 
 var NSEC_PER_SEC = 1000000000;
 var register = 3.1415;
+var logs = document.getElementById('logs');
 
 function square_am_signal(time, freq) {
-	document.getElementById('logs').value += "Playing / " + time + " seconds / " + freq + "Hz\n";
+	window.logs.value += "\nPlaying / " + time + " seconds / " + freq + "Hz";
 	var period = NSEC_PER_SEC / freq;
 	var start = now();
 	var end = now() + time * NSEC_PER_SEC;
@@ -33,13 +34,19 @@ function start() {
 	var line, time, freq;
 	for (var i = 0; i < length; i++) {
 		line = song[i].split(" ");
-		if (line[1] == "0") {
-			// delay
+		if (+line[1] == 0) {
+			pause(line[0]);
 		}
 		else {
 			freq = +line[1];
-			time = +line[0];
+			time = (+line[0])*.001;
 			square_am_signal(time, freq);
 		}
 	}
+}
+
+function pause(time) {
+	window.logs.value += "\nPaused / " + time*.001 + " seconds";
+	var dt = new Date();
+	while ((new Date()) - dt <= time) { /* Do nothing */ }
 }
