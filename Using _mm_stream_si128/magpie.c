08262feb75c76f00fffd0f46ba186d2630683586
Mycroft <mycroft.tovarish@gmail.com>
2018-02-20 22:17:33 -0800

    Update magpie.c

--- Using _mm_stream_si128/magpie.c
+++ Using _mm_stream_si128/magpie.c
@@ -50,7 +50,7 @@ mach_port_t clock_port;
 mach_timespec_t remain;
 
 static inline void square_am_signal(float time, float frequency) {
-    printf("Playing / %0.3f seconds / %4.0f Hz\n", time, frequency);
+    //printf("Playing / %0.3f seconds / %4.0f Hz\n", time, frequency);
     uint64_t period = NSEC_PER_SEC / frequency;
 
     uint64_t start = mach_absolute_time();
@@ -71,10 +71,70 @@ static inline void showhelp(char *argv[]){
             fprintf(stderr, "usage: %s [-m|a] [-l loops] -f fname\n",argv[0]);
             fprintf(stderr, "magpie - an AM-FSK file transmitter in the 1500khzrange\n\n Uses PWM of square waves to generate AM-AFSK transmission of a file \n\nbased on SBR by William Entriken:https://github.com/fulldecent\n\n");
             char uc[] = "  -l|loops [int] \t loops sets the number of times the data is transmitted. \n  -f [filename] \t the file specified in filename is read and transmitted\n";
-            fprintf(stderr,uc);
+            fprintf(stderr,"%s",uc);
             char uc2[] = "  -m\t\t\t uses (3)FSK repeat per bit with a FC/FC+300hz/FC+450hz encoding\n  -a\t\t\t uses AFSK encoding (0=1200hz, 1=2200hz)\n\n";
-            fprintf(stderr,uc2);
+            fprintf(stderr,"%s",uc2);
 }
+
+static inline void preamble(){
+fprintf(stdout, "\n0,1,1,0,");
+square_am_signal(1.0 * 200 /2 / 1000, 1200);
+square_am_signal(1.0 * 200 /2 / 1000, 2200);
+square_am_signal(1.0 * 200 /2 / 1000, 2200);
+square_am_signal(1.0 * 200 /2 / 1000, 1200);
+return;
+}
+
+static inline void afskbit(char b, int time_ms){
+    int freq_hz = 0;
+    if(b == 1){
+        fprintf(stdout, "1,");
+        freq_hz = 1200;
+        }
+    else {
+        freq_hz = 2200;
+        fprintf(stdout, "0,");
+        }
+        square_am_signal(1.0 * time_ms / 1000, freq_hz);
+}
+
+static inline void mfsknib(char nib, int time_ms){
+    //read byte in, and split to bit array.
+        int freq=0;
+        switch(nib){
+        case 0:
+        freq=200;
+        break;
+        case 1:
+        freq=200*2;
+        break;
+        case 2:
+        freq=200*3;
+        break;
+        case 3:
+        freq=200*5;
+        break;
+        case 4:
+        freq=200*7;
+        break;
+        case 5:
+        freq=200*11;
+        break;
+        case 6:
+        freq=200*13;
+        break;
+        case 7:
+        freq=200*19;
+        break;
+        }
+        square_am_signal(1.0 * time_ms /2 / 1000, freq);
+}
+
+char makenib(char mb_0, char mb_1, char mb_2){
+return (mb_0 + (mb_1 << 1) + (mb_2 << 2));
+
+}
+
 static inline void fileplayer(char *file, int loops, int enc){
 FILE *fileptr;
 char *buffer;
@@ -92,31 +152,39 @@ fclose(fileptr); // Close the file
 char bitnow=0;
 
 int time_ms=200;
-int freq_hz;
+//int freq_hz;
 
-int i,b;
+//int i,b;
 for ( int j = 0; j < loops; j ++){
-
 for ( int i = 0; i < filelen + 1; i++) {
-    printf("byte #%d\n",i);
-    for ( int b =0; b < 7; b++) {
+    char mb_0=0;
+    char mb_1=0;
+    char mb_2=0;
+
+    for ( int b =0; b < 7; b++) { 
     bitnow = (buffer[i] & ( 1 << b )) >> b ;
-    if(bitnow == 1) 
-        freq_hz = 1200;
-    else
-        freq_hz = 2200;
-    if(enc == 1) {
-        square_am_signal(1.0 * time_ms / 1000, freq_hz);
-        }
+    
+    if (enc == 1){
+            afskbit(bitnow, time_ms); 
+            if( i  %  2 == 0 ){
+                //preamble every 16 bits
+                preamble();
+            }
+    }
     else {
-        square_am_signal(1.0 * time_ms /2 / 1000, freq_hz);
-        square_am_signal(1.0 * time_ms /2 / 1000, freq_hz+(2*150));
-        square_am_signal(1.0 * time_ms /2 / 1000, freq_hz+(3*150));
+            //rolling 3 bit windows, used to determine 8FSK frequency.
+            mb_2 = bitnow & 1;
+            char nib = makenib(mb_0, mb_1, mb_2);
+            mfsknib(nib, time_ms);
+            mb_0=mb_1 & 1;
+            mb_1=mb_2 & 1;
         }
     }
 }
+fprintf(stdout, "\n");
 }
 }
+
 
 int
 main(int argc, char *argv[ ])
@@ -125,7 +193,6 @@ main(int argc, char *argv[ ])
     int tflg=0, eflg=0, errflg = 0;
     char *file = NULL;
     int loops=1;
-    //char *loops;
     extern char *optarg;
     extern int optind, optopt;
     while ((c = getopt(argc, argv, ":hmaf:l:")) != -1) {
@@ -172,7 +239,6 @@ main(int argc, char *argv[ ])
         fprintf(stderr, "usage: %s [-m|a] [-l loops] -f fname\n",argv[0]);
         exit(2);
     }
-//printf("infile: %s loops:%i tflag:%i eflag:%i\n" ,file, loops,tflg,eflg);
 //Setup below
 
 #ifdef __MACH__
